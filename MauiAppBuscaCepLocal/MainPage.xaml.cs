06f97e8a3ffec15bc5f9bb19fee7b34ca367468d
Tiago <frost.tiago@gmail.com>
2024-08-29 15:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppBuscaCepLocal/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MauiAppBuscaCepLocal.Model;
+using MauiAppBuscaCepLocal.Services;
+
+namespace MauiAppBuscaCepLocal
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Endereco? dados_endereco = await DataServiceCep.GetEnderecoByCep(txt_cep.Text);
+
+                BindingContext = dados_endereco;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+
+        }
+    }
+
+}
